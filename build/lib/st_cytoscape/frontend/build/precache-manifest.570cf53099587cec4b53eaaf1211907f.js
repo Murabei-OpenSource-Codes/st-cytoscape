a676bd8c2ec497a3682fce33b6953645095ba6e2
Gabriel <gabriel.batista@murabei.com>
2025-05-23 12:31:26 -0300

    add frontend build output

---
+++ build/lib/st_cytoscape/frontend/build/precache-manifest.570cf53099587cec4b53eaaf1211907f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a426612f797223d094ebeb54cbe4ac1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "708a4bfd944321f2a61c",
+    "url": "./static/js/2.55843620.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.55843620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0319db4fb92df6845d4c",
+    "url": "./static/js/main.8125c52f.chunk.js"
+  },
+  {
+    "revision": "4f647358ff6c1f15178c",
+    "url": "./static/js/runtime-main.db64ee98.js"
+  }
+]);
