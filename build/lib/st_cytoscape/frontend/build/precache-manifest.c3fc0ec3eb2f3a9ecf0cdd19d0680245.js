ec58f9aee484cc7002236d3efedf6586c3a1dce2
Gabriel <gabriel.batista@murabei.com>
2025-09-04 18:17:28 -0300

    Building a new version 0.1.1

---
+++ build/lib/st_cytoscape/frontend/build/precache-manifest.c3fc0ec3eb2f3a9ecf0cdd19d0680245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b554cf372d488ec74fd849456ab0a684",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5691be08cdbf79565be3",
+    "url": "./static/js/2.9a6f4ed3.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.9a6f4ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7999b73903af4229b28",
+    "url": "./static/js/main.f3e1cb64.chunk.js"
+  },
+  {
+    "revision": "4f647358ff6c1f15178c",
+    "url": "./static/js/runtime-main.db64ee98.js"
+  }
+]);
